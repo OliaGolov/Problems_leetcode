4f4e4d15dcde3d889c2d27706af679e834f947a1
Olga <ompavlukova@gmail.com>
2024-09-04 10:01:08 -0700

    add easyLevel and mediumLevel folders with problems

---
+++ easyLevel/1_two_sum_indices_map.js
@@ -0,0 +1,15 @@
+var twoSum = function (nums, target) {
+  let mapOfNumbers = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (mapOfNumbers.has(complement)) {
+      return [mapOfNumbers.get(complement), i];
+    }
+
+    mapOfNumbers.set(nums[i], i);
+  }
+};
+
+console.log(twoSum([1, 3, 7, 11, 14], 10));
---
+++ easyLevel/9_palindrome_number.js
@@ -0,0 +1,15 @@
+let x = '121'
+let y = -121
+
+let backwordX = ''
+let i = 0
+for (i = x.length - 1; i >= 0; i--) {
+    
+    backwordX +=x[i]
+}
+
+for (let i = 0; i < x.length -1; i++) {
+    for (let j = 0; j < x.length -1; j++) {
+        if ()
+    }
+}
