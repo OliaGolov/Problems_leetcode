b8081080ea35cd26c5e926e5a4503991b9cc9a2b
Olga <ompavlukova@gmail.com>
2024-09-09 15:57:49 -0700

    add 20_Valid Parentheses file

--- easyLevel/20_validParanthesis.js
+++ easyLevel/20_validParanthesis.js
@@ -1,39 +1,23 @@
-const s = "["
-let newArr = []
-const arr =Array.from(s)
+var isValid = function(s) {
+  let balance = []
 
-function closingBracket(closing) {
-    if (closing == ')') {
-        return '('
-    }
-    if (closing == '}') {
-        return '{'
-    }
-    if (closing == ']') {
-        return '['
-    }   
+    for (let i = 0; i < s.length; i++) {
+      if(s[i] == '{') {
+        balance.push('}')
+      }
+      else if(s[i] == '[') {
+        balance.push(']')
+      }
+      else if(s[i] == '(') {
+        balance.push(')')
+      }
+      else if (balance.pop() !== s[i]) {
+          return false
+      }
+  }
+
+  return !balance.length
 }
 
-for (i=0; i < arr.length; i++) {
-    if (arr[i] == '(' || arr[i] == '[' || arr[i] == '{') {
-        newArr.push(arr[i]);
-    }
-    else { 
-        if (closingBracket(arr[i]) == newArr[newArr.length-1]) {
-        newArr.pop()
-        }
-        else {
-            console.log(false);
-            newArr.push(arr[i])
-            break;
-        }    
-    }
-}
+ console.log(isValid('[['));
 
-if (newArr.length == 0) {
-        console.log(true);
-    }
-else {
-    console.log(false);
-}
-    
