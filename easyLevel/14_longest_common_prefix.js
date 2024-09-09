d8f12c89aa6facde26d3cd174252469f7b00bd6a
Olga <ompavlukova@gmail.com>
2024-09-08 20:16:36 -0700

    add 14_linked lists core file

--- easyLevel/14_longest_common_prefix.js
+++ easyLevel/14_longest_common_prefix.js
@@ -1,13 +1,16 @@
 
 var longestCommonPrefix = function(strs) {
+    
+    if (strs.length === 0) return ''
+    if (strs.length === 1) return strs
 
     for(let i = 0; i < strs[0].length; i++) {
         for(let j = 1; j < strs.length; j++){
             if(strs[0][i] !== strs[j][i]){
-
                 return strs[0].slice(0,i)
             }
         }
+        
     }
 }
-console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix([""]));
