774958f8f6b9fe0f161865d19d910ea49eae7428
Olga <ompavlukova@gmail.com>
2024-09-19 11:24:06 -0700

    update 58_length_of_last_word.js file

--- easyLevel/58_length_of_last_word.js
+++ easyLevel/58_length_of_last_word.js
@@ -1,27 +1,22 @@
-var lengthOfLastWord = function(s) {
-
-    function deleteSpaces(s) {
-        for(let i = s.length-1; i >= 0; i-- ) {
-            let str = ''
-            while(s[i] != ' ') {
-                str = s.slice(0, i+1)
-                return str
-            }
-        }
+var lengthOfLastWord = function (s) {
+  function deleteSpaces(s) {
+    for (let i = s.length - 1; i >= 0; i--) {
+      let str = "";
+      while (s[i] != " ") {
+        str = s.slice(0, i + 1);
+        return str;
+      }
     }
+  }
 
-    let trimmedStr = deleteSpaces(s)
-    
-    
-    for(let j = trimmedStr.length-1; j >= 0; j-- ) {
-       
-        if (trimmedStr[j] == ' ') {
-            trimmedStr = trimmedStr.slice(j+1, trimmedStr.length)
-        }
-        
+  let trimmedStr = deleteSpaces(s);
+
+  for (let j = trimmedStr.length - 1; j >= 0; j--) {
+    if (trimmedStr[j] == " ") {
+      trimmedStr = trimmedStr.slice(j + 1, trimmedStr.length);
     }
-    return trimmedStr.length
-
+  }
+  return trimmedStr.length;
 };
 
 console.log(lengthOfLastWord("moon"));
